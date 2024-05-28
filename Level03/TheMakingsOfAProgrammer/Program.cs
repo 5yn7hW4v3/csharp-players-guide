489be7318a7c38ed0e3f5edb6066ce5ec596cc83
5n7hW4v3 <73319380+5yn7hW4v3@users.noreply.github.com>
2024-05-27 20:44:15 -0500

    The Makings of a Programmer

---
+++ Level03/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hi there!");
+Console.WriteLine("My name is 5n7hW4v3.");
+Console.WriteLine("I ... I am pretty new to C#.");
+Console.WriteLine("But hopefully I can help you fight the Uncoded One");
+Console.WriteLine("Let the adventure begin!");

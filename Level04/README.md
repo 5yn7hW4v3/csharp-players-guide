b976f79d7fe4a5c83e11ea9d3d464e91a1ac745c
5n7hW4v3 <73319380+5yn7hW4v3@users.noreply.github.com>
2024-05-28 19:30:01 -0500

    The Thing Namer 3000

---
+++ Level04/README.md
@@ -0,0 +1,8 @@
+# LEVEL 4
+## COMMENTS
+
+**Speedrun**
+
+- Comments let you put text in a program that the computer ignores. They can provide information
+to help programmers understand or remember what the code does.
+- Anything after two slashes (//) on a line is a comment, as is anything between /* and */.
---
+++ Level04/TheThingNamer3000/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What kind of thing are we talking about?");
+
+// string A sets the "thing" that the user is talking about
+string a = Console.ReadLine();
+
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+
+/* string B sets the description of the "thing"
+    that the user is talking about*/
+string b = Console.ReadLine();
+string c = " of Doom";
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + c + " " + d + "!");

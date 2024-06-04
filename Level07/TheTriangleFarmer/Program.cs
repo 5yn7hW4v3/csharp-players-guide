ac903793e248d5c06864d72d9c3c709bbf925806
5n7hW4v3 <73319380+5yn7hW4v3@users.noreply.github.com>
2024-06-03 21:23:48 -0500

    The Triangle Farmer

---
+++ Level07/TheTriangleFarmer/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string triangleBaseString;
+string triangleHeightString;
+double triangleBaseDouble;
+double triangleHeightDouble;
+double triangleArea;
+
+
+Console.Write("Triangle Base: ");
+triangleBaseString = Console.ReadLine();
+Console.Write("Triangle Height: ");
+triangleHeightString = Console.ReadLine();
+
+triangleBaseDouble = Convert.ToDouble(triangleBaseString);
+triangleHeightDouble = Convert.ToDouble(triangleHeightString);
+
+triangleArea = (triangleBaseDouble * triangleHeightDouble) / 2;
+
+Console.WriteLine($"Triangle Area: {triangleArea}");

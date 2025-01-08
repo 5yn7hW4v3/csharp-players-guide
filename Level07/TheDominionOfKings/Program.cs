9825a485d2361783aef57721095e3e530103299f
5yn7hW4v3 <73319380+5yn7hW4v3@users.noreply.github.com>
2025-01-07 19:16:19 -0600

    The Dominion of Kings
    Level 07 - The Dominion of Kings

---
+++ Level07/TheDominionOfKings/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int estate = 1;
+int duchy = 3;
+int province = 6;
+
+Console.Write("Enter the number of Estates: ");
+String estateString = Console.ReadLine();
+Console.Write("Enter the number of Duchies: ");
+String duchyString = Console.ReadLine();
+Console.Write("Enter the number of Provinces: ");
+String provinceString = Console.ReadLine();
+
+int estateInt = Convert.ToInt32(estateString);
+int duchyInt = Convert.ToInt32(duchyString);
+int provinceInt = Convert.ToInt32(provinceString);
+
+int totalPoints = (estate * estateInt) + (duchy * duchyInt) + (province * provinceInt);
+
+Console.WriteLine($"Total Points: {totalPoints}");

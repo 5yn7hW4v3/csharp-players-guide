6c5c9ce49313856628a4416460b5df9eb5496844
5n7hW4v3 <73319380+5yn7hW4v3@users.noreply.github.com>
2025-02-03 21:08:12 -0600

    Level 08 The Defense of Consolas

---
+++ Level08/TheDefenseOfConsolas/TheDefenseOfConsolas/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Target Row? ");
+string targetRowString = Console.ReadLine();
+Console.Write("Target Column? ");
+string targetColumnString = Console.ReadLine();
+
+int targetRowInt = Convert.ToInt32(targetRowString);
+int targetColumnInt = Convert.ToInt32(targetColumnString);
+
+int deployLeft = targetColumnInt - 1;
+int deployRight = targetColumnInt + 1;
+int deployUp = targetRowInt + 1;
+int deployDown = targetRowInt - 1;
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine($"Deploy to:\n({targetRowInt},{deployLeft})\n({targetColumnInt},{deployDown})\n({targetRowInt},{deployRight})\n({deployUp},{targetColumnInt})");
+Console.Beep();
